0936ada340e95ab0245bf4f03dfc520e4f8ecc11
mirzacckc <cicekcimirzacan@gmail.com>
2022-09-10 20:06:58 +0300

    Update table-accordion.component.ts

--- src/app/shared/components/table-accordion/table-accordion.component.ts
+++ src/app/shared/components/table-accordion/table-accordion.component.ts
@@ -1,8 +1,7 @@
-
 import { Component, Input, OnInit } from '@angular/core';
 import { BillingAccount } from 'src/app/features/customers/models/billingAccount';
 import { Offer } from 'src/app/features/offers/models/offer';
-import {  MessageService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 import { CustomersService } from 'src/app/features/customers/services/customer/customers.service';
 import { Customer } from 'src/app/features/customers/models/customer';
 import { Router } from '@angular/router';
@@ -15,77 +14,91 @@ import { Router } from '@angular/router';
 export class TableAccordionComponent implements OnInit {
   @Input() billingAccount!: BillingAccount;
   @Input() customerId!: number;
-  customer!:Customer;
-  billingAccountToDelete!:BillingAccount;
+  customer!: Customer;
+  billingAccountToDelete!: BillingAccount;
 
-  constructor(private messageService:MessageService,private customerService:CustomersService,private router:Router) {}
+  constructor(
+    private messageService: MessageService,
+    private customerService: CustomersService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.getCustomerById()
+    this.getCustomerById();
     this.messageService.clearObserver.subscribe((data) => {
       if (data == 'reject') {
         this.messageService.clear();
       } else if (data == 'confirm') {
-        if(this.billingAccountToDelete.orders && this.billingAccountToDelete.orders.length>0)
-        {
-          this.messageService.clear(); 
+        if (
+          this.billingAccountToDelete.orders &&
+          this.billingAccountToDelete.orders.length > 0
+        ) {
+          this.messageService.clear();
           this.messageService.add({
             key: 'offer',
             severity: 'warn',
-            detail: 'There is a product belonging to the account, this account cannot be deleted',
+            detail:
+              'There is a product belonging to the account, this account cannot be deleted',
           });
           setTimeout(() => {
-          this.messageService.clear();           
-          }, 3000);         
-        }
-        else{     
-          this.messageService.clear(); 
+            this.messageService.clear();
+          }, 3000);
+        } else {
+          this.messageService.clear();
           this.messageService.add({
             key: 'offer',
             severity: 'warn',
             detail: 'Customer account deleted successfully',
-          });    
+          });
           setTimeout(() => {
-            this.messageService.clear();           
-            }, 3000); 
+            this.messageService.clear();
+          }, 3000);
           this.remove();
-        }       
+        }
       }
     });
   }
 
-  productDetail(billingAccount:BillingAccount,offer:Offer){
-      if(offer.type.typeName == 'campaign'){
-        let cnAddress = billingAccount.addresses.toString();
-        let cnProdOfferId = offer.id.toString();
-        let cnProdOfferName = offer.name;
-        let cnCampaignId = offer.type.id.toString();        
-        this.messageService.add({
-          key: 'okey',            
-          sticky: true,
-          severity: 'warn',
-          detail:
-          'ProdOfferId:'+cnProdOfferId+'                '+
-          'ProdOfferName:'+cnProdOfferName+'   '+
-          'CampaignId'+cnCampaignId,            
-        });  
-      }
-      else if(offer.type.typeName == 'catalog'){
-        let cgAddress = billingAccount.addresses;
-        let cgProdOfferId = offer.id;
-        let cgProdOfferName = offer.name;
-        this.messageService.add({
-          key: 'okey',            
-          sticky: true,
-          severity: 'warn',
-          detail:
-          'ProdOfferId:'+cgProdOfferId+'                '+
-          'ProdOfferName:'+cgProdOfferName+'   '            
-        });
-      }
+  productDetail(billingAccount: BillingAccount, offer: Offer) {
+    if (offer.type.typeName == 'campaign') {
+      let cnAddress = billingAccount.addresses.toString();
+      let cnProdOfferId = offer.id.toString();
+      let cnProdOfferName = offer.name;
+      let cnCampaignId = offer.type.id.toString();
+      this.messageService.add({
+        key: 'okey',
+        sticky: true,
+        severity: 'warn',
+        detail:
+          'ProdOfferId:' +
+          cnProdOfferId +
+          '                ' +
+          'ProdOfferName:' +
+          cnProdOfferName +
+          '   ' +
+          'CampaignId' +
+          cnCampaignId,
+      });
+    } else if (offer.type.typeName == 'catalog') {
+      let cgAddress = billingAccount.addresses;
+      let cgProdOfferId = offer.id;
+      let cgProdOfferName = offer.name;
+      this.messageService.add({
+        key: 'okey',
+        sticky: true,
+        severity: 'warn',
+        detail:
+          'ProdOfferId:' +
+          cgProdOfferId +
+          '                ' +
+          'ProdOfferName:' +
+          cgProdOfferName +
+          '   ',
+      });
+    }
   }
 
-  getCustomerById() {       
+  getCustomerById() {
     if (this.customerId == undefined) {
       //toast
     } else {
@@ -97,7 +110,6 @@ export class TableAccordionComponent implements OnInit {
     }
   }
 
-  
   removePopup(billinAccount: BillingAccount) {
     this.billingAccountToDelete = billinAccount;
     this.messageService.add({
@@ -108,14 +120,19 @@ export class TableAccordionComponent implements OnInit {
     });
   }
 
-  remove() {            
+  remove() {
     this.customerService
       .removeBillingAccount(this.billingAccountToDelete, this.customer)
       .subscribe((data) => {
         setTimeout(() => {
-          window.location.reload()
+          window.location.reload();
         }, 3000);
-       
-      });      
+      });
+  }
+
+  updateBillingAccount(billingAccount: BillingAccount) {
+    this.router.navigateByUrl(
+      `/dashboard/customers/${this.customerId}/customer-bill/update/${billingAccount.id}`
+    );
   }
 }
